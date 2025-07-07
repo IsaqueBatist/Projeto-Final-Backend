03e5665d99ab39f2757d8aebcfed43fecadd12b7
Isaque Barbosa <isaquebatista400@gmail.com>
2025-07-07 01:36:21 -0300

    :sparkles: feat: added Relations between entities

--- src/main/java/br/gov/sp/itu/fatec/frontend/entities/Email.java
+++ src/main/java/br/gov/sp/itu/fatec/frontend/entities/Email.java
@@ -1,6 +1,9 @@
 package br.gov.sp.itu.fatec.frontend.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,6 +15,8 @@ import lombok.Setter;
 @EqualsAndHashCode(of = "id")
 @Table(name = "TBL_EMAILS")
 public class Email {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String label;
   private String email;
